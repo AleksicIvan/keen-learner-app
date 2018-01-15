3d1665f2c74fd0af5254b3c0f54a7c94c67eb156
Ivan Aleksic <ivan.aleksic@guidefitter.com>
2018-01-15 12:21:57 +0100

    Remove readig dir code from server.js

--- server.js
+++ server.js
@@ -2,22 +2,6 @@ const express = require('express')
 const app = express()
 const fs = require('fs')
 const cors = require('cors')
-const uuidv4 = require('uuid/v4')
-
-var videoList = []
-const videoExt = /\.(webm|mp4|ogg)$/
-
-fs.readdir(__dirname, function(err, items) {
-  if (err) {
-    return
-  }
-
-  videoList = items
-    .filter(item => videoExt.test(item))
-    .map((v) => {
-      return { name: `/${v}`, id: uuidv4(), watched: false }
-    })
-})
 
 app.use(cors())
 app.get('/', function (req, res) {
