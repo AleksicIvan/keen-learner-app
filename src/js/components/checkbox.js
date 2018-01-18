6a6cce4a7b2672fc0d9da1bfe3a8baaeaebac862
Ivan Aleksic <ivan.aleksic@guidefitter.com>
2018-01-18 06:20:49 +0100

    Add checked prop to checkbox

--- src/js/components/checkbox.js
+++ src/js/components/checkbox.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const Checkbox = ({ isWatched, isWatchedHandler, id }) => (
-  <input type="checkbox"
-    value={ isWatched }
+const Checkbox = ({ isWatched, isWatchedHandler, id }) => {
+  return <input type="checkbox"
+    checked={ isWatched }
     onChange={ v => isWatchedHandler(v, id) }
   />
-)
+}
 
 Checkbox.propTypes = {
   isWatched: PropTypes.bool,
