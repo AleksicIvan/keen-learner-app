50dd3de86277644095ebeadefee4c66c3a41b278
Ivan Aleksic <ivan.aleksic@guidefitter.com>
2018-01-28 09:28:32 +0100

    Remove unused poster prop

--- src/js/components/video.js
+++ src/js/components/video.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import ReactPlayer from 'react-player'
 
-import loader from '../../assets/Loading_icon.gif'
-
 export default class Video extends Component {
   constructor(props) {
     super(props)
@@ -23,11 +21,10 @@ export default class Video extends Component {
       <ReactPlayer className='react-player'
         width='100%'
         height='100%'
-        poster={ this.state.ready ? null : loader }
         url={ this.props.videoModal.src }
         controls
         onReady={ () => this.handleOnLoad() }
-        onEnded={ () => this.props.isWatchedHandler(true, this.props.videoModal.videoId) } >
+        onEnded={ () => this.props.isWatchedHandler(true, this.props.videoModal.videoId) }>
       </ReactPlayer>
     </div>
   }
