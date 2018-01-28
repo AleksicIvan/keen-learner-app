4080028019df46153fe938a58d8d7014d5d87eb4
Ivan Aleksic <ivan.aleksic@guidefitter.com>
2018-01-28 09:30:37 +0100

    Refactor VideoModal to be stateless component

--- src/js/components/modals/videoModal.js
+++ src/js/components/modals/videoModal.js
@@ -1,5 +1,4 @@
 import React from 'react'
-// import Modal from 'react-modal'
 import PropTypes from 'prop-types'
 import get from 'lodash/get'
 import { Modal } from 'semantic-ui-react'
@@ -8,40 +7,25 @@ import Checkbox from '../checkbox'
 import Video from '../video'
 
 
-class VideoModal extends React.Component {
-  state: {
-    watched: null
-  }
+const VideoModal = ({
+  videoModal,
+  isWatchedHandler,
+  closeModal
+}) => {
+  return <div>
+    <Modal open={ videoModal.isOpen } size="large" onClose={ () => closeModal() } closeIcon closeOnDimmerClick>
+      <Modal.Content>
+        <Video videoModal={ videoModal } isWatchedHandler={ isWatchedHandler } />
+        <br />
 
-  componentWillReceiveProps (nextProps) {
-    if (this.props.videoModal.watched !== nextProps.videoModal.watched) {
-      this.setState({
-        watched: nextProps.videoModal.watched
-      })
-    }
-  }
-  render () {
-    const videoModal = this.props.videoModal
-    const isWatchedHandler = this.props.isWatchedHandler
-    const closeModal = this.props.closeModal
+        <Checkbox label='Watched'
+          isWatched={ videoModal.watched || false }
+          isWatchedHandler={ isWatchedHandler }
+          id={ get(videoModal, 'videoId') } />
+      </Modal.Content>
+    </Modal>
 
-
-    return <div>
-      <Modal open={ videoModal.isOpen } size="large" onClose={ () => closeModal() } closeIcon closeOnDimmerClick>
-        <Modal.Content>
-          <Video videoModal={ videoModal } isWatchedHandler={ isWatchedHandler } />
-          <br />
-
-          <Checkbox label='Watched'
-            isWatched={ get(this.state, 'watched', false) }
-            isWatchedHandler={ isWatchedHandler }
-            id={ get(videoModal, 'videoId') } />
-        </Modal.Content>
-      </Modal>
-
-    </div>
-
-  }
+  </div>
 }
 
 
@@ -49,7 +33,6 @@ VideoModal.propTypes = {
   videoModal: PropTypes.object,
   closeModal: PropTypes.func,
   isWatchedHandler: PropTypes.func,
-  isWatched: PropTypes.bool,
 }
 
 export default VideoModal
