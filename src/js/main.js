9ab741d97f04961dee6d08e268ffce44c08b8dc5
Ivan Aleksic <ivan.aleksic@guidefitter.com>
2018-02-06 16:50:04 +0100

    Import update modaul from ramda/es

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-import { update } from 'ramda'
+import { update } from 'ramda/es'
 
 import '../css/style.css'
 // import keenImage from '../assets/keen.png'
