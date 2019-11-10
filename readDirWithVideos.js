b0f298b32e20eca7e026e1c7c142a973bc825ff9
Ivan Aleksic <ivan.aleksic@guidefitter.com>
2019-11-10 18:08:12 +0100

    Add sorting algorith for read videos

--- readDirWithVideos.js
+++ readDirWithVideos.js
@@ -1,29 +1,34 @@
-const fs = require('fs')
 const uuidv4 = require('uuid/v4')
 const level = require('level')
+const read = require('fs-readdir-recursive')
 
 let videoList = []
 const db = level('./videolistDb')
 const videoExt = /\.(webm|mp4|ogg)$/
 
-fs.readdir(__dirname, function(err, items) {
+const compareNumbers = (a, b) => {
+  let _a = parseInt(a)
+  let _b = parseInt(b)
+  return _a - _b
+}
+
+let filz = read(`${__dirname}/videos`) || []
+
+videoList = filz
+  .sort(compareNumbers)
+  .map((v) => {
+    return { name: `/videos/${v}`, id: uuidv4(), watched: false }
+  }) || []
+
+
+let data = {
+  data: videoList
+    .filter(f => videoExt.test(f.name))
+}
+
+
+let stringifiedData = JSON.stringify(data, null, ' ')
+db.put('data', stringifiedData, err => {
   if (err) throw err
-
-  videoList = items
-    .filter(item => videoExt.test(item))
-    .map((v) => {
-      return { name: `/${v}`, id: uuidv4(), watched: false }
-    })
-
-  let data = {
-    data: videoList
-  }
-
-  let stringifiedData = JSON.stringify(data, null, ' ')
-
-  db.put('data', stringifiedData, err => {
-    if (err) throw err
-    console.log('DB update success')
-  })
+  console.log('DB update success')
 })
-
