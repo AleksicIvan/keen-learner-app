30559308dc3971450523a0d535c9f41c0f50b1e2
Ivan Aleksic <ivan.aleksic@guidefitter.com>
2019-11-10 17:59:41 +0100

    Bump level package and add sorting algorithm for read videos

--- readDirWithVideos.js
+++ readDirWithVideos.js
@@ -6,20 +6,28 @@ let videoList = []
 const db = level('./videolistDb')
 const videoExt = /\.(webm|mp4|ogg)$/
 
+const compareNumbers = (a, b) => {
+  let _a = parseInt(a)
+  let _b = parseInt(b)
+  return _a - _b
+}
 
 let filz = read(`${__dirname}/videos`) || []
 
-videoList = filz.map((v) => {
-  return { name: `/videos/${v}`, id: uuidv4(), watched: false }
-}) || []
+videoList = filz
+  .sort(compareNumbers)
+  .map((v) => {
+    return { name: `/videos/${v}`, id: uuidv4(), watched: false }
+  }) || []
+
 
 let data = {
-  data: videoList.filter(f => videoExt.test(f.name))
+  data: videoList
+    .filter(f => videoExt.test(f.name))
 }
 
 
 let stringifiedData = JSON.stringify(data, null, ' ')
-
 db.put('data', stringifiedData, err => {
   if (err) throw err
   console.log('DB update success')
